549c5231a19ced4e641de6432acc357538baeefa
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2020-12-13 17:52:50 +0900

    CartItemCongtroller@storeの編集

--- ecsite2/app/Http/Controllers/CartItemController.php
+++ ecsite2/app/Http/Controllers/CartItemController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\CartItem;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CartItemController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        CartItem::updateOrCreate( // updateOrCreateはレコードの登録と更新を兼ねるメソッド
+            [
+                'user_id' => Auth::id(), // ログインしているユーザー
+                'item_id' => $request->post('item_id'), // 商品ID
+            ],
+            [
+                'quantity' => \DB::raw('quantity + ' . $request->post('quantity')), // 数量
+            ]
+        );
+        return redirect('/');
+    }
 }
