319c7ae0a2c59030f2097fb682de13461f6bc942
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2020-12-13 18:48:13 +0900

    CartItemモデルの編集

--- ecsite2/app/CartItem.php
+++ ecsite2/app/CartItem.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class CartItem extends Model
 {
-    //
+    protected $fillable =
+    [
+        'user_id',
+        'item_id',
+        'quantity',
+    ];
 }
